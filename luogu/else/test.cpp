b7d372cc693fad88157634e127e7a93bc2c812ce
koweiyi <1423376854@qq.com>
2023-04-27 02:12:02 +0800

    c++ vscode 配置

--- luogu/else/test.cpp
+++ luogu/else/test.cpp
@@ -1,38 +1,45 @@
 #include <bits/stdc++.h>
-#define mem(a,b) memset(a,b,sizeof a)
-#define rep(i,n,m) for(int i=n;i<=m;i++)
-#define pre(i,n,m) for(int i=n;i>=m;i--)
-#define len(x) (int) (x).size()
+#define mem(a, b) memset(a, b, sizeof a)
+#define rep(i, n, m) for (int i = n; i <= m; i++)
+#define pre(i, n, m) for (int i = n; i >= m; i--)
+#define len(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) a/gcd(a,b)*b
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) a / gcd(a, b) * b
 #define ft first
 #define sd second
 #define PI acos(-1.0)
 #define INF 0x3f3f3f3f
 #define ll long long
 #define endl "\n"
-#define pii pair<int,int>
+#define pii pair<int, int>
 using namespace std;
 const int maxn = 100005;
 
-inline ll read(void) {
+inline ll read(void)
+{
     int s = 0, w = 1;
     char ch = getchar();
-    for(; ch < '0' || ch > '9'; ch = getchar()) if(ch == '-') w = -1;
-    for(; ch <= '9' && ch >= '0'; ch = getchar()) s= s * 10 + ch - '0';
+    for (; ch < '0' || ch > '9'; ch = getchar())
+        if (ch == '-')
+            w = -1;
+    for (; ch <= '9' && ch >= '0'; ch = getchar())
+        s = s * 10 + ch - '0';
     return s * w;
 }
 
-inline void out(ll a){
-    if (a >= 10) out(a / 10);
+inline void out(ll a)
+{
+    if (a >= 10)
+        out(a / 10);
     putchar(a % 10 + '0');
 }
 
-
-int main(int argc, char const *argv[]){
-    ios::sync_with_stdio(0),cin.tie(0);
-
-    
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
     return 0;
 }
