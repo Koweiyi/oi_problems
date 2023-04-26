e8087c41f0b6e9d70e57c8f3b7a3ef5d97f65e34
koweiyi <1423376854@qq.com>
2023-04-27 02:18:17 +0800

    删除配置信息

--- luogu/else/test.cpp
+++ luogu/else/test.cpp
@@ -40,6 +40,7 @@ int main(int argc, char const *argv[])
     ios::sync_with_stdio(0), cin.tie(0);
     int a, b;
     cin >> a >> b;
-    cout << a + b << endl;
+    int x = a + b;
+    cout << x << endl;
     return 0;
 }
