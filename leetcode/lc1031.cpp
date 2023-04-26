2ad6f1e7b4ed160257e5b2f0cd3e6c55d6c9fae2
koweiyi <1423376854@qq.com>
2023-04-27 02:41:23 +0800

    配置文件更改

--- leetcode/lc1031.cpp
+++ leetcode/lc1031.cpp
@@ -1,21 +1,30 @@
 /*
+ * @Author: Koweiyi 1423376854@qq.com
+ * @Date: 2023-04-26 15:37:19
+ * @LastEditors: koweiyi 1423376854@qq.com
+ * @LastEditTime: 2023-04-27 02:34:17
+ * @FilePath: /oi_problems/leetcode/lc1031.cpp
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+/*
  * @lc app=leetcode.cn id=1031 lang=cpp
  * @lcpr version=21907
  *
  * [1031] 两个非重叠子数组的最大和
  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-        
+    int maxSumTwoNoOverlap(vector<int> &nums, int firstLen, int secondLen)
+    {
     }
 };
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -31,4 +40,3 @@ public:
 // @lcpr case=end
 
  */
-
