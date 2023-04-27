6b5edccf0b5e8a6eb05535b87f1037f822931ae5
Koweiyi <1423376854@qq.com>
2023-04-27 15:00:33 +0800

    单调队列模板题

---
+++ leetcode/lc239.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=cpp
+ * @lcpr version=21907
+ *
+ * [239] 滑动窗口最大值
+ *
+ * https://leetcode.cn/problems/sliding-window-maximum/description/
+ *
+ * algorithms
+ * Hard (49.73%)
+ * Likes:    2272
+ * Dislikes: 0
+ * Total Accepted:    433.3K
+ * Total Submissions: 871.2K
+ * Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+ *
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
+ * 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回 滑动窗口中的最大值 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * ⁠1 [3  -1  -3] 5  3  6  7       3
+ * ⁠1  3 [-1  -3  5] 3  6  7       5
+ * ⁠1  3  -1 [-3  5  3] 6  7       5
+ * ⁠1  3  -1  -3 [5  3  6] 7       6
+ * ⁠1  3  -1  -3  5 [3  6  7]      7
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ *
+ *
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    {
+        vector<int> res;
+        int n = nums.size();
+        deque<int> q;
+        for(int i = 0 ; i < n ; i ++){
+            while(!q.empty() && nums[i] > nums[q.back()]) q.pop_back();
+            q.push_back(i);
+            while(q.front() < i - k + 1) q.pop_front();
+            if(i >= k - 1){
+                res.push_back(nums[q.front()]);
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,3,-1,-3,5,3,6,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+ */
