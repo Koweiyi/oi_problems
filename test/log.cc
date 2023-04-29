6e1010b2e4d67de5c47e7d3e1aedc34c4b8ee9ce
Koweiyi <1423376854@qq.com>
2023-04-30 00:25:27 +0800

    del somethings

--- test/log.cc
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-
-void Log(const char* s){
-    cout << s << endl;
-}
--- test/test.cc
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-extern void Log(const char* message);
-
-int main(){
-    auto s = "hello koweiyi";
-    Log(s);
-    return 0;
-}
