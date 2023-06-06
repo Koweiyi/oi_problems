ef8893ba7a51a777cdcef279818f83ff3c003e1f
Koweiyi <1423376854@qq.com>
2023-06-06 14:27:32 +0800

    base data structures

---
+++ algo/1-base-datastructures/2-queue/lg2422.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define mem(a,b) memset(a,b,sizeof a)
+#define rep(i,n,m) for(int i=n;i<m;i++)
+#define repp(i,n,m) for(int i=n;i<=m;i++)
+#define pre(i,n,m) for(int i=n;i>=m;i--)
+#define len(x) (int) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) a/gcd(a,b)*b
+#define ft first
+#define sd second
+#define PI acos(-1.0)
+#define INF 0x3f3f3f3f
+#define ll long long
+#define endl "\n"
+#define pii pair<int,int>
+using namespace std;
+
+inline ll read(void) {
+    int s = 0, w = 1;
+    char ch = getchar();
+    for(; ch < '0' || ch > '9'; ch = getchar()) if(ch == '-') w = -1;
+    for(; ch <= '9' && ch >= '0'; ch = getchar()) s= s * 10 + ch - '0';
+    return s * w;
+}
+
+inline void out(ll a){
+    if (a >= 10) out(a / 10);
+    putchar(a % 10 + '0');
+}
+
+const int maxn = 100005;
+ll a[maxn], sum[maxn], l[maxn];
+int main(int argc, char const *argv[]){
+    ios::sync_with_stdio(0),std::cin.tie(0);
+    ll n;
+    std::cin >> n;
+    repp(i, 1, n){
+        std::cin >> a[i];
+        sum[i] = sum[i - 1] + a[i];
+    }
+    ll res = 0;
+    stack<ll> s;
+    repp(i, 1, n + 1){
+        while(!s.empty() && a[i] < a[s.top()]){
+            res = max(res, (sum[i - 1] - sum[l[s.top()]]) * a[s.top()]);
+            s.pop();
+        }
+        l[i] = s.empty() ? 0 : s.top();
+        s.push(i);
+    }
+    std::cout << res << endl;
+    return 0;
+}
