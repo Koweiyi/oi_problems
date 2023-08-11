bd0d60771586f9ffb4f56cabb979a876365e8583
Koweiyi <1423376854@qq.com>
2023-08-11 20:29:49 +0800

    weekly332 Q2 solved

---
+++ leetcode/weekly/w332/lc2563.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=2563 lang=golang
+ * @lcpr version=21913
+ *
+ * [2563] 统计公平数对的数目
+ *
+ * https://leetcode.cn/problems/count-the-number-of-fair-pairs/description/
+ *
+ * algorithms
+ * Medium (34.06%)
+ * Likes:    36
+ * Dislikes: 0
+ * Total Accepted:    7.4K
+ * Total Submissions: 21.6K
+ * Testcase Example:  '[0,1,7,4,4,5]\n3\n6'
+ *
+ * 给你一个下标从 0 开始、长度为 n 的整数数组 nums ，和两个整数 lower 和 upper ，返回 公平数对的数目 。
+ *
+ * 如果 (i, j) 数对满足以下情况，则认为它是一个 公平数对 ：
+ *
+ *
+ * 0 <= i < j < n，且
+ * lower <= nums[i] + nums[j] <= upper
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [0,1,7,4,4,5], lower = 3, upper = 6
+ * 输出：6
+ * 解释：共计 6 个公平数对：(0,3)、(0,4)、(0,5)、(1,3)、(1,4) 和 (1,5) 。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1,7,9,2,5], lower = 11, upper = 11
+ * 输出：1
+ * 解释：只有单个公平数对：(2,3) 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * nums.length == n
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= lower <= upper <= 10^9
+ *
+ *
+ */
+package leetcode
+
+import "sort"
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+// @lc code=start
+func countFairPairs(nums []int, lower int, upper int) int64 {
+	sort.Ints(nums)
+	var res int64 = 0
+	for j := 0 ; j < len(nums) ; j ++ {
+		l := sort.SearchInts(nums[:j],  lower - nums[j])
+		r := sort.SearchInts(nums[:j], upper - nums[j] + 1)
+		res += int64(r - l)
+
+	}
+	return res 
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [0,1,7,4,4,5]\n3\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,7,9,2,5]\n11\n11\n
+// @lcpr case=end
+
+ */
+
