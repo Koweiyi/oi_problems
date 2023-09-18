468914da1a77841a017a7913bc1d14aa2922c665
Koweiyi <1423376854@qq.com>
2023-09-18 15:23:31 +0800

    牛客周赛r13

---
+++ newcoder/周赛/r13/d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define mem(a,b) memset(a,b,sizeof a)
+#define rep(i,n,m) for(int i=n;i<m;i++)
+#define repp(i,n,m) for(int i=n;i<=m;i++)
+#define pre(i,n,m) for(int i=n;i>=m;i--)
+#define len(x) (int) (x).size()
+#define all(x) (x).begin(), (x).end()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) a/gcd(a,b)*b
+#define ft first
+#define sd second
+#define PI acos(-1.0)
+#define INF 0x3f3f3f3f
+#define ll long long
+#define endl "\n"
+#define pii pair<int,int>
+using namespace std;
+
+inline ll read(void) {
+    int s = 0, w = 1;
+    char ch = getchar();
+    for(; ch < '0' || ch > '9'; ch = getchar()) if(ch == '-') w = -1;
+    for(; ch <= '9' && ch >= '0'; ch = getchar()) s= s * 10 + ch - '0';
+    return s * w;
+}
+
+inline void out(ll a){
+    if (a >= 10) out(a / 10);
+    putchar(a % 10 + '0');
+}
+
+const int N = 100005;
+int a[N];
+int main(int argc, char const *argv[]){
+    ios::sync_with_stdio(0),std::cin.tie(0);
+
+    int n;
+    n = read();
+    repp(i, 1, n) a[i] = read();
+    
+    return 0;
+}
