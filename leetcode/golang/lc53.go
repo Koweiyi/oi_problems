42fef66a85a83d0782ac80b44c439b5afa750b37
Koweiyi <1423376854@qq.com>
2023-11-20 10:36:44 +0800

    2023-11-20每日一题

---
+++ leetcode/golang/lc53.go
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=golang
+ * @lcpr version=30110
+ *
+ * [53] 最大子数组和
+ *
+ * https://leetcode.cn/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Medium (54.95%)
+ * Likes:    6428
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 2.8M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 子数组 是数组中的一个连续部分。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1]
+ * 输出：1
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ *
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ *
+ */
+
+// @lcpr-template-start
+package leetcode
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// @lcpr-template-end
+// @lc code=start
+func maxSubArray(nums []int) int {
+	res := math.MinInt32 
+	pre := 0 
+	for i := 0 ; i < len(nums) ; i ++ {
+		pre = nums[i] + max(0, pre)
+		res = max(res, pre)
+	}
+	return res 
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [-2,1,-3,4,-1,2,1,-5,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,4,-1,7,8]\n
+// @lcpr case=end
+
+*/
